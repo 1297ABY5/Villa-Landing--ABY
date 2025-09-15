d96559787e4557a7cb37d3194078f461e2b36eac
1297ABY5 <1297aby5@gmail.com>
2025-09-15 18:16:39 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     return () => clearInterval(timerId);
   }, [timeLeft]);
 
-  const formatTime = (seconds: number) => {
+  const formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
     return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
