bf58683e5c8db87a8551d944e51791091734ff27
1297ABY5 <1297aby5@gmail.com>
2025-09-15 18:55:05 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"], // later add domains for external images
+    domains: [
+      "images.unsplash.com",
+      "randomuser.me"
+    ],
   },
 };
 
