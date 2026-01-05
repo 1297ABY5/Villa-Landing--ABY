74c83622a23057c1a65388e281ff05578d7e0893
1297ABY5 <1297aby5@gmail.com>
2026-01-05 14:48:57 +0400

    Update InstagramFeed.js

--- components/InstagramFeed.js
+++ components/InstagramFeed.js
@@ -2,24 +2,25 @@ import { useEffect } from 'react';
 
 export default function InstagramFeed() {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://static.elfsight.com/platform/platform.js';
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    
-    return () => {
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
-    };
+    // Prevent loading the script multiple times
+    if (!document.querySelector('script[src="https://static.elfsight.com/platform/platform.js"]')) {
+      const script = document.createElement('script');
+      script.src = 'https://static.elfsight.com/platform/platform.js';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+    }
   }, []);
 
   return (
-    <div 
-      className="elfsight-app-be187cad-c36b-4712-b333-d86a66d2da6d rounded-xl overflow-hidden"
-      data-elfsight-app-lazy
+    <div
+      className="rounded-xl overflow-hidden"
       style={{ minHeight: '400px' }}
-    />
+    >
+      <div
+        className="elfsight-app-6382b6ca-a83d-4bf1-ab58-16d8558954a4"
+        data-elfsight-app-lazy
+      ></div>
+    </div>
   );
 }
