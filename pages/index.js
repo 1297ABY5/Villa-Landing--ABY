538618ba316ccbfb22eded52d0abdcec47dbb1db
1297ABY5 <1297aby5@gmail.com>
2026-01-22 11:34:44 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1881,6 +1881,9 @@ export default function LuxuryQSOptimizer({ initialContent, initialServices }) {
 
         {/* DEFERRED ANALYTICS */}
         <script dangerouslySetInnerHTML={{ __html: `window.addEventListener('load',function(){setTimeout(function(){var s=document.createElement('script');s.src='https://www.googletagmanager.com/gtag/js?id=AW-612864132';s.async=true;document.head.appendChild(s);s.onload=function(){window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}window.gtag=gtag;gtag('js',new Date());gtag('config','AW-612864132');};},1500);});` }} />
+        
+        {/* Microsoft Clarity Analytics */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "v5bkaisuew");` }} />
       </div>
     </>
   );
